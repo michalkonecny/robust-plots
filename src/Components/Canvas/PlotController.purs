14e237da65bdf8fdb57991d039d24cd4849de30f
RelativeForce <joshiceddy@hotmail.com>
2020-05-22 12:05:24 +0100

    Updated gridlines to use decimal

--- src/Components/Canvas/PlotController.purs
+++ src/Components/Canvas/PlotController.purs
@@ -4,13 +4,14 @@ import Prelude
 import Components.Canvas.Commands (DrawCommand)
 import Components.Canvas.Commands.Actions (drawXGridLine, drawYGridLine, drawPolygon)
 import Components.Canvas.Plot (Plot(..))
+import Data.Decimal as D
 import Data.Array ((..))
 import Data.Either (Either(..))
 import Data.Foldable (for_)
-import Data.Int (floor, toNumber, ceil)
+import Data.Int (ceil, toNumber)
 import Effect (Effect)
 import Effect.Aff (Aff, Canceler, Error, makeAff, nonCanceler)
-import Math (log, pow, round)
+import Math (log, pow)
 import Types (XYBounds)
 
 computePlotAsync :: Plot -> Aff (DrawCommand Unit)
@@ -30,19 +31,30 @@ drawXGridLines :: XYBounds -> DrawCommand Unit
 drawXGridLines bounds = for_ xGuidePoints draw
   where
   range = bounds.xBounds.upper - bounds.xBounds.lower
-  interval = range / 20.0
-  x1 = bounds.xBounds.lower
-  xGuidePoints = map (\p -> (((toNumber p) * range) / 20.0) + x1) $ 0 .. 20
 
-  draw :: Number -> DrawCommand Unit
-  draw xi = drawXGridLine xiD xi range
+  interval = range / 20.0
+
+  x1 = bounds.xBounds.lower
+
+  xGuidePoints = map toGuidePoints $ 0 .. 20
+
+  draw :: { x :: Number, value :: Number } -> DrawCommand Unit
+  draw { x, value } = drawXGridLine x value range
+
+  toGuidePoints :: Int -> { x :: Number, value :: Number }
+  toGuidePoints index = { x, value }
     where
-    xiD = xi - bounds.xBounds.lower
+    numberIndex = toNumber index
+
+    value = D.toNumber $ D.toSignificantDigits 3 $ D.fromNumber $ ((numberIndex * range) / 20.0) + x1
+
+    x = (numberIndex * range) / 20.0
 
 drawYGridLines :: XYBounds -> DrawCommand Unit
 drawYGridLines bounds = for_ yGuidePoints draw
   where
   range = bounds.yBounds.upper - bounds.yBounds.lower
+
   gran = pow 10.0 $ log $ range / 10.0
 
   y1 = bounds.yBounds.lower
