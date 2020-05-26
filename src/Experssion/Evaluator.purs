6212d10f4dedcb82a27f1fab3b1b95dd240bac34
RelativeForce <joshiceddy@hotmail.com>
2020-05-26 17:22:54 +0100

    Added Evaluator

---
+++ src/Experssion/Evaluator.purs
@@ -0,0 +1,88 @@
+module Plot.Expression.Evaluator where
+
+import Prelude
+
+import Data.Array (find)
+import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..))
+import Math (cos, e, exp, log, pi, pow, sin, sqrt, tan)
+import Plot.Expression.Error (Expect, unknownValue)
+import Plot.Expression.Syntax (BinaryOperation(..), Constant(..), Expression(..), UnaryOperation(..), VariableName)
+
+type VariableMap a = Array (Tuple VariableName a)
+
+lookup :: forall a. VariableMap a -> VariableName -> Maybe a
+lookup variableMap variableName = toMaybeValue $ find search variableMap
+  where 
+    search :: Tuple VariableName a -> Boolean
+    search (Tuple name _) = name == variableName
+
+    toMaybeValue :: Maybe (Tuple VariableName a) -> Maybe a
+    toMaybeValue (Just (Tuple _ value)) = Just value
+    toMaybeValue _ = Nothing
+
+
+evaluate :: VariableMap Number -> Expression -> Expect Expression
+evaluate variableMap = case _ of
+  expression@(ExpressionLiteral value) -> pure expression
+  ExpressionConstant constant -> evaluateConstant constant
+  ExpressionVariable name -> case lookup variableMap name of
+    Just value -> pure $ ExpressionLiteral value
+    _ -> unknownValue name
+  ExpressionBinary operation leftExpression rightExpression -> evaluateBinaryOperation operation variableMap leftExpression rightExpression
+  ExpressionUnary operation expression -> evaluateUnaryOperation operation variableMap expression
+
+evaluateBinaryOperation :: BinaryOperation -> VariableMap Number -> Expression -> Expression -> Expect Expression
+evaluateBinaryOperation Plus = evaluateArithmeticBinaryOperation add
+evaluateBinaryOperation Minus = evaluateArithmeticBinaryOperation sub
+evaluateBinaryOperation Times = evaluateArithmeticBinaryOperation mul
+evaluateBinaryOperation Divide = evaluateArithmeticBinaryOperation div
+evaluateBinaryOperation Power = evaluateArithmeticBinaryOperation pow
+
+evaluateArithmeticBinaryOperation :: forall s. ((Semiring s) => s -> s -> s) -> VariableMap Number -> Expression -> Expression -> Expect Expression
+evaluateArithmeticBinaryOperation operation variableMap (ExpressionLiteral leftValue) (ExpressionLiteral rightValue) = pure $ ExpressionLiteral $ operation leftValue rightValue
+evaluateArithmeticBinaryOperation operation variableMap leftExpression rightExpression = do
+    leftValue <- evaluate variableMap leftExpression
+    rightValue <- evaluate variableMap rightExpression
+    evaluateArithmeticBinaryOperation operation variableMap leftValue rightValue
+
+evaluateConstant :: Constant -> Expect Expression
+evaluateConstant (Pi) = pure $ ExpressionLiteral pi
+evaluateConstant (E) = pure $ ExpressionLiteral e
+
+evaluateUnaryOperation :: UnaryOperation -> VariableMap Number -> Expression -> Expect Expression
+evaluateUnaryOperation (Neg) = evaluateNegate 
+evaluateUnaryOperation (Sqrt) = evaluateSquareRoot 
+evaluateUnaryOperation (Exp) = evaluateExponential
+evaluateUnaryOperation (Log) = evaluateLogarithm
+evaluateUnaryOperation (Sine) = evaluateSine
+evaluateUnaryOperation (Cosine) = evaluateCosine
+evaluateUnaryOperation (Tan) = evaluateTan
+
+evaluateNegate :: VariableMap Number -> Expression -> Expect Expression
+evaluateNegate variableMap (ExpressionLiteral value) = pure $ ExpressionLiteral $ -value
+evaluateNegate variableMap expression = evaluate variableMap expression >>= evaluateNegate variableMap
+
+evaluateSquareRoot :: VariableMap Number -> Expression -> Expect Expression
+evaluateSquareRoot variableMap (ExpressionLiteral value) = pure $ ExpressionLiteral $ sqrt value
+evaluateSquareRoot variableMap expression = evaluate variableMap expression >>= evaluateSquareRoot variableMap
+
+evaluateExponential :: VariableMap Number -> Expression -> Expect Expression
+evaluateExponential variableMap (ExpressionLiteral value) = pure $ ExpressionLiteral $ exp value
+evaluateExponential variableMap expression = evaluate variableMap expression >>= evaluateExponential variableMap
+
+evaluateLogarithm :: VariableMap Number -> Expression -> Expect Expression
+evaluateLogarithm variableMap (ExpressionLiteral value) = pure $ ExpressionLiteral $ log value
+evaluateLogarithm variableMap expression = evaluate variableMap expression >>= evaluateLogarithm variableMap
+
+evaluateSine :: VariableMap Number -> Expression -> Expect Expression
+evaluateSine variableMap (ExpressionLiteral value) = pure $ ExpressionLiteral $ sin value
+evaluateSine variableMap expression = evaluate variableMap expression >>= evaluateSine variableMap
+
+evaluateCosine :: VariableMap Number -> Expression -> Expect Expression
+evaluateCosine variableMap (ExpressionLiteral value) = pure $ ExpressionLiteral $ cos value
+evaluateCosine variableMap expression = evaluate variableMap expression >>= evaluateCosine variableMap
+
+evaluateTan :: VariableMap Number -> Expression -> Expect Expression
+evaluateTan variableMap (ExpressionLiteral value) = pure $ ExpressionLiteral $ tan value
+evaluateTan variableMap expression = evaluate variableMap expression >>= evaluateTan variableMap
