1388fedd9566d6b946e2fd58eb18604a582b3516
mikkonecny <mikkonecny@gmail.com>
2020-05-27 18:47:34 +0100

    Tests that Dyadic is a commutative ring

--- test/IntervalArith/Dyadic.purs
+++ test/IntervalArith/Dyadic.purs
@@ -1,18 +1,34 @@
 module Test.IntervalArith.Dyadic where
 
 import Prelude
-
 import IntervalArith.Dyadic (Dyadic, (:^))
 import IntervalArith.Misc (scale)
 import Test.IntervalArith.Misc (ArbitraryInteger(..), ArbitraryPositiveExponent(..))
+import Test.Ring (commutativeRingTests)
 import Test.QuickCheck (class Arbitrary, arbitrary, (==?))
 import Test.QuickCheck.Gen (chooseInt, sized)
 import Test.Unit (TestSuite, suite, test)
 import Test.Unit.QuickCheck (quickCheck)
 
+newtype ArbitraryDyadic
+  = ArbitraryDyadic Dyadic
+
+instance arbitraryDyadic :: Arbitrary ArbitraryDyadic where
+  arbitrary =
+    sized \size -> do
+      (ArbitraryInteger a) <- arbitrary
+      s <- chooseInt (-2 * size) (2 * size)
+      pure $ ArbitraryDyadic $ a :^ s
+
 dyadicTests :: TestSuite
 dyadicTests =
   suite "IntervalArith.Misc - Dyadic arithmetic" do
+    dyadicTests_Scaling
+    dyadicTests_Ring
+
+dyadicTests_Scaling :: TestSuite
+dyadicTests_Scaling =
+  suite "IntervalArith.Misc - Dyadic scaling by powers of 2" do
     test "SHOULD HOLD n = scale (scale n i) (-i) FOR ALL integers n and i>=0"
       $ quickCheck \dPre iPre ->
           let
@@ -28,12 +44,28 @@ dyadicTests =
             expected = d
           in
             expected ==? result
+    test "SHOULD HOLD n = scale (scale n (-i)) i FOR ALL integers n and i>=0"
+      $ quickCheck \dPre iPre ->
+          let
+            -- given
+            (ArbitraryDyadic d) = dPre :: ArbitraryDyadic
 
-newtype ArbitraryDyadic
-  = ArbitraryDyadic Dyadic
+            (ArbitraryPositiveExponent i) = iPre
 
-instance arbitraryDyadic :: Arbitrary ArbitraryDyadic where
-  arbitrary = sized \size -> do
-    (ArbitraryInteger a) <- arbitrary
-    s <- chooseInt (-2*size) (2*size)
-    pure $ ArbitraryDyadic $ a :^ s
+            -- when
+            result = scale (scale d (-i)) i
+
+            -- then
+            expected = d
+          in
+            expected ==? result
+
+dyadicTests_Ring :: TestSuite
+dyadicTests_Ring =
+  commutativeRingTests
+    { suitePrefix: "IntervalArith.Misc - Dyadic"
+    , valuesName: "dyadic numbers"
+    , fromArbitraryValue: \(ArbitraryDyadic d) -> d
+    , eqOp: (==?)
+    , eqOpSymbol: "="
+    }
