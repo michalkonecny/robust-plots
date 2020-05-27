c30db5d8e0ad38872aca9d88f4c566c3437827a1
RelativeForce <joshiceddy@hotmail.com>
2020-05-27 15:18:39 +0100

    Added tests for VariableMap

--- test/Expression/Evaluator/Evaluator.purs
+++ test/Expression/Evaluator/Evaluator.purs
@@ -2,9 +2,12 @@ module Test.Expression.Evaluator
   ( evaluatorTests
   ) where
 
+import Prelude
 import Test.Unit (TestSuite)
 import Test.Expression.Evaluator.Evaluate (evaluateTests)
+import Test.Expression.Evaluator.Lookup (lookUpTests)
 
 evaluatorTests :: TestSuite
 evaluatorTests = do
   evaluateTests
+  lookUpTests
