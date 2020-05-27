199e1e203b04d88d4532a6c5f3e96c0f33e49dfb
mikkonecny <mikkonecny@gmail.com>
2020-05-27 18:47:18 +0100

    Commutative ring generic tests

---
+++ test/TestUtils.purs
@@ -0,0 +1,17 @@
+module Test.TestUtils where
+
+import Prelude
+
+import Test.QuickCheck (Result, (<?>))
+
+type SuiteEqParams1 at t
+  = { suitePrefix :: String
+    , valuesName :: String
+    , fromArbitraryValue :: (at -> t)
+    , eqOp :: (t -> t -> Result)
+    , eqOpSymbol :: String
+    }
+
+-- copied from source code of purescript-quickcheck Test.QuickCheck.
+assertOp :: forall a. Eq a => Show a => (a -> a -> Boolean) -> String -> a -> a -> Result
+assertOp op failString a b = a `op` b <?> show a <> failString <> show b
