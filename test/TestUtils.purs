3682981cec70403167d30d604b7e0e426eaa34a1
mikkonecny <mikkonecny@gmail.com>
2020-05-29 00:17:32 +0100

    test Dyadic <= is a total order

--- test/TestUtils.purs
+++ test/TestUtils.purs
@@ -12,6 +12,16 @@ type SuiteEqParams1 at t
     , eqOpSymbol :: String
     }
 
+type SuiteOrdParams1 at t
+  = { suitePrefix :: String
+    , valuesName :: String
+    , fromArbitraryValue :: (at -> t)
+    , leqOp :: (t -> t -> Result)
+    , leqOpSymbol :: String
+    , eqOp :: (t -> t -> Result)
+    , eqOpSymbol :: String
+    }
+
 -- copied from source code of purescript-quickcheck Test.QuickCheck.
 assertOp :: forall a. Eq a => Show a => (a -> a -> Boolean) -> String -> a -> a -> Result
 assertOp op failString a b = a `op` b <?> show a <> failString <> show b
