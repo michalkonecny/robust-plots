d968f6e32ea8d21ba59f1ac203ec22e24e67b3e1
mikkonecny <mikkonecny@gmail.com>
2020-05-29 00:17:32 +0100

    test Dyadic <= is a total order

---
+++ test/Order.purs
@@ -0,0 +1,99 @@
+module Test.Order where
+
+import Prelude
+import Test.QuickCheck (class Arbitrary)
+import Test.QuickCheck.Combinators ((&=&), (==>))
+import Test.TestUtils (SuiteOrdParams1)
+import Test.Unit (TestSuite, suite, test)
+import Test.Unit.QuickCheck (quickCheck)
+
+totalOrderTests ::
+  forall at t. Arbitrary at => CommutativeRing t => SuiteOrdParams1 at t -> TestSuite
+totalOrderTests params = do
+  suite (params.suitePrefix <> " is a total order") do
+    reflexivity params
+    transitivity params
+    antisymmetry params
+
+partialOrderTests ::
+  forall at t. Arbitrary at => CommutativeRing t => SuiteOrdParams1 at t -> TestSuite
+partialOrderTests params =
+  suite (params.suitePrefix <> " is a partial order") do
+    reflexivity params
+    transitivity params
+
+reflexivity ::
+  forall at t. Arbitrary at => CommutativeRing t => SuiteOrdParams1 at t -> TestSuite
+reflexivity { suitePrefix, valuesName, fromArbitraryValue, leqOp, leqOpSymbol, eqOp, eqOpSymbol } =
+  test
+    ( "SHOULD HOLD reflexivity: "
+        <> "a"
+        <> leqOpSymbol
+        <> "a"
+        <> " FOR ALL "
+        <> valuesName
+        <> " a"
+    )
+    $ quickCheck \aA ->
+        let
+          a = fromArbitraryValue aA
+        in
+          (a) `leqOp` (a)
+
+antisymmetry ::
+  forall at t. Arbitrary at => CommutativeRing t => SuiteOrdParams1 at t -> TestSuite
+antisymmetry { suitePrefix, valuesName, fromArbitraryValue, leqOp, leqOpSymbol, eqOp, eqOpSymbol } =
+  test
+    ( "SHOULD HOLD antisymmetry: "
+        <> "a"
+        <> leqOpSymbol
+        <> "b"
+        <> " and "
+        <> "b"
+        <> leqOpSymbol
+        <> "a"
+        <> " implies "
+        <> "a"
+        <> eqOpSymbol
+        <> "b"
+        <> " FOR ALL "
+        <> valuesName
+        <> " a,b"
+    )
+    $ quickCheck \aA bA ->
+        let
+          a = fromArbitraryValue aA
+
+          b = fromArbitraryValue bA
+        in
+          ((a `leqOp` b) &=& (b `leqOp` a)) ==> (a `eqOp` b)
+
+transitivity ::
+  forall at t. Arbitrary at => CommutativeRing t => SuiteOrdParams1 at t -> TestSuite
+transitivity { suitePrefix, valuesName, fromArbitraryValue, leqOp, leqOpSymbol, eqOp, eqOpSymbol } =
+  test
+    ( "SHOULD HOLD transitivity: "
+        <> "a"
+        <> leqOpSymbol
+        <> "b"
+        <> " and "
+        <> "b"
+        <> leqOpSymbol
+        <> "c"
+        <> " implies "
+        <> "a"
+        <> leqOpSymbol
+        <> "c"
+        <> " FOR ALL "
+        <> valuesName
+        <> " a,b,c"
+    )
+    $ quickCheck \aA bA cA ->
+        let
+          a = fromArbitraryValue aA
+
+          b = fromArbitraryValue bA
+
+          c = fromArbitraryValue cA
+        in
+          ((a `leqOp` b) &=& (b `leqOp` c)) ==> (a `leqOp` c)
