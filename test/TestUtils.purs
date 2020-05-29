6513ce8c129bcdba6820191c9465f61ee2de5fc6
mikkonecny <mikkonecny@gmail.com>
2020-05-29 15:39:22 +0100

    tool for reporting inputs in test failures

--- test/TestUtils.purs
+++ test/TestUtils.purs
@@ -2,6 +2,7 @@ module Test.TestUtils where
 
 import Prelude
 
+import Data.String (joinWith)
 import Test.QuickCheck (Result, (<?>))
 
 type SuiteEqParams1 at t
@@ -24,4 +25,27 @@ type SuiteOrdParams1 at t
 
 -- copied from source code of purescript-quickcheck Test.QuickCheck.
 assertOp :: forall a. Eq a => Show a => (a -> a -> Boolean) -> String -> a -> a -> Result
-assertOp op failString a b = a `op` b <?> show a <> failString <> show b
+assertOp op failString a b =
+  a `op` b
+    <?> show a
+    <> failString
+    <> show b
+
+-- copied from source code of purescript-quickcheck Test.QuickCheck.
+assertOpMoreHelp :: forall a. Eq a => Show a => (a -> a -> Boolean) -> String -> String -> a -> a -> Result
+assertOpMoreHelp op opString moreHelp a b =
+  a `op` b
+    <?> show a
+    <> opString
+    <> show b
+    <> moreHelp
+
+assertOpWithInput :: 
+  forall t1 t2. Eq t1 => Show t1 => Show t2 => 
+  (t1 -> t1 -> Boolean) -> String -> Array t2 -> t1 -> t1 -> Result
+assertOpWithInput op opString input = assertOpMoreHelp op opString moreHelp
+  where
+  moreHelp = "; " <> (joinWith ", " $ map show input)
+
+eqWithInput :: forall t1 t2. Eq t2 => Show t2 => Show t1 => Array t1 -> t2 -> t2 -> Result
+eqWithInput = assertOpWithInput (==) " == "
