1708d40f3d90afbc743ea937763db14b83db3b9e
RelativeForce <joshiceddy@hotmail.com>
2020-06-02 09:29:21 +0100

    Added leaf node cases

--- src/Expression/Simplifier.purs
+++ src/Expression/Simplifier.purs
@@ -1,20 +1,30 @@
 module Expression.Simplifier where
 
 import Prelude
+
 import Expression.Syntax (BinaryOperation(..), Expression(..), UnaryOperation(..))
 
 simplify :: Expression -> Expression
 simplify (ExpressionUnary operation expression) = case simplify expression, operation of
   ExpressionLiteral value, Neg -> ExpressionLiteral (-value)
-  foldedExpression, _ -> ExpressionUnary operation foldedExpression
+  simplifiedExpression, _ -> ExpressionUnary operation simplifiedExpression
 
 simplify (ExpressionBinary Power leftExpression (ExpressionLiteral 1.0)) = simplify leftExpression
 
 simplify (ExpressionBinary operation leftExpression rightExpression) = case simplify leftExpression, simplify rightExpression, operation of
+  -- Trim zero leaf nodes
+  ExpressionLiteral 0.0, simplifiedRightExpression, Times -> ExpressionLiteral 0.0
+  simplifiedLeftExpression, ExpressionLiteral 0.0, Times -> ExpressionLiteral 0.0
+  ExpressionLiteral 0.0, simplifiedRightExpression, Plus -> simplifiedRightExpression
+  simplifiedLeftExpression, ExpressionLiteral 0.0, Plus -> simplifiedLeftExpression
+  ExpressionLiteral 0.0, simplifiedRightExpression, Minus -> ExpressionUnary Neg simplifiedRightExpression
+  simplifiedLeftExpression, ExpressionLiteral 0.0, Minus -> simplifiedLeftExpression
+  ExpressionLiteral 0.0, simplifiedRightExpression, Divide -> ExpressionLiteral 0.0
+  -- Simplify constant leaf nodes
   ExpressionLiteral leftValue, ExpressionLiteral rightValue, Plus -> ExpressionLiteral (leftValue + rightValue)
   ExpressionLiteral leftValue, ExpressionLiteral rightValue, Minus -> ExpressionLiteral (leftValue - rightValue)
   ExpressionLiteral leftValue, ExpressionLiteral rightValue, Times -> ExpressionLiteral (leftValue * rightValue)
   ExpressionLiteral leftValue, ExpressionLiteral rightValue, Divide -> ExpressionLiteral (leftValue / rightValue)
-  foldedLeftExpression, foldedRightExpression, _ -> ExpressionBinary operation foldedLeftExpression foldedRightExpression
+  simplifiedLeftExpression, simplifiedRightExpression, _ -> ExpressionBinary operation simplifiedLeftExpression simplifiedRightExpression
 
 simplify expression = expression
