c670531206bceece59896c5e46ef8bb9c1ac08b9
mikkonecny <mikkonecny@gmail.com>
2020-06-08 00:14:20 +0100

    test Approx "better" pre-order

--- test/IntervalArith/Approx/Approx.purs
+++ test/IntervalArith/Approx/Approx.purs
@@ -2,9 +2,44 @@ module Test.IntervalArith.Approx
   ( approxTests
   ) where
 
+import Prelude
+
+import IntervalArith.Approx (Approx(..), approxMB, (⊑))
+import IntervalArith.Misc (big)
+import Test.IntervalArith.Approx.ShowA (approxTests_showA)
+import Test.IntervalArith.Misc (ArbitraryInteger(..))
+import Test.Order (partialOrderTests, preOrderTests)
+import Test.QuickCheck (class Arbitrary, arbitrary)
+import Test.QuickCheck.Gen (chooseInt, sized)
+import Test.TestUtils (SuiteOrdParams1, assertOpWithInput)
 import Test.Unit (TestSuite)
-import Test.IntervalArith.Approx.ShowA (showATests)
+
+data ArbitraryApprox = ArbitraryApprox Approx
+
+instance arbitraryApprox :: Arbitrary ArbitraryApprox where
+  arbitrary =
+    sized \size -> do
+      (ArbitraryInteger m) <- arbitrary
+      s <- chooseInt (-2 * size) (2 * size)
+      mb <- chooseInt 2 (10 + 2 * size)
+      pure $ ArbitraryApprox $ approxMB mb m (big 0) s
 
 approxTests :: TestSuite
 approxTests = do
-  showATests
+  approxTests_showA
+  approxTests_Order
+
+approxTests_Order :: TestSuite
+approxTests_Order = preOrderTests approxOrdParams
+  
+
+approxOrdParams :: SuiteOrdParams1 ArbitraryApprox Approx
+approxOrdParams =
+  { suitePrefix: "IntervalArith.Approx ⊑"
+  , valuesName: "interval approximations"
+  , fromArbitraryValue: \(ArbitraryApprox d) -> d
+  , leqOpWithInput: (assertOpWithInput (⊑) " ⊑ ")
+  , leqOpSymbol: "⊑"
+  , eqOpWithInput: (assertOpWithInput (==) " == ")
+  , eqOpSymbol: "="
+  }
