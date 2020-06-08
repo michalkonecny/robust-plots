4b4435d4c1232066405a1bf377ef9e651a935fe9
mikkonecny <mikkonecny@gmail.com>
2020-06-09 00:03:25 +0100

    test setMB may make a only worse

--- test/IntervalArith/Approx/Approx.purs
+++ test/IntervalArith/Approx/Approx.purs
@@ -5,16 +5,17 @@ module Test.IntervalArith.Approx
 import Prelude
 
 import Effect (Effect)
-import IntervalArith.Approx (Approx, approxMB, (⊑))
+import IntervalArith.Approx (Approx, approxMB, better, setMB, (⊑))
 import IntervalArith.Approx.ShowA (showA)
 import IntervalArith.Misc (big)
 import Test.IntervalArith.Approx.ShowA (approxTests_showA)
-import Test.IntervalArith.Misc (ArbitraryInteger(..))
+import Test.IntervalArith.Misc (ArbitraryInteger(..), ArbitraryPositiveExponent(..))
 import Test.Order (preOrderTests)
 import Test.QuickCheck (class Arbitrary, arbitrary)
 import Test.QuickCheck.Gen (Size, chooseInt, randomSample', sized)
-import Test.TestUtils (SuiteOrdParams1, assertOpWithInput)
-import Test.Unit (TestSuite)
+import Test.TestUtils (SuiteOrdParams1, assertOp, assertOpWithInput)
+import Test.Unit (TestSuite, suite, test)
+import Test.Unit.QuickCheck (quickCheck)
 
 data ArbitraryApprox
   = ArbitraryApprox Approx
@@ -28,13 +29,30 @@ instance arbitraryApprox :: Arbitrary ArbitraryApprox where
       pure $ ArbitraryApprox $ approxMB mb m (big 0) s
 
 randomSampleApprox :: Size -> Effect (Array String)
-randomSampleApprox size = 
-  randomSample' size (map (\(ArbitraryApprox a) -> a) arbitrary) >>= \as -> pure (map showA as)
+randomSampleApprox size = randomSample' size (map (\(ArbitraryApprox a) -> a) arbitrary) >>= \as -> pure (map showA as)
 
 approxTests :: TestSuite
 approxTests = do
   approxTests_showA
+  approxTests_setMBworse
   approxTests_Order
+
+approxTests_setMBworse :: TestSuite
+approxTests_setMBworse =
+  suite "IntervalArith.Approx - setMB" do
+    test "SHOULD HOLD setMB mb a ⊒ a FOR ALL approx a and integer mb>=0"
+      $ quickCheck \aPre mbPre ->
+          let
+            -- given
+            (ArbitraryApprox a) = aPre
+
+            (ArbitraryPositiveExponent mb) = mbPre
+
+            -- when
+            aMB = setMB mb a
+          -- then
+          in
+            assertOp (flip better) " ⊒ " aMB a
 
 approxTests_Order :: TestSuite
 approxTests_Order = preOrderTests approxOrdParams
