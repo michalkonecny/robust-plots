57149c62b3d9cbfc4f9a62751ce9acc1b5ffb1dc
RelativeForce <joshiceddy@hotmail.com>
2020-06-09 13:35:05 +0100

    Refactored simplifer

--- src/Expression/Simplifier.purs
+++ src/Expression/Simplifier.purs
@@ -4,17 +4,14 @@ import Prelude
 
 import Control.Alt ((<|>))
 import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Ratio (denominator, numerator)
 import Expression.Syntax (BinaryOperation(..), Expression(..), UnaryOperation(..))
-import Math (pow)
+import IntervalArith.Misc (big, toRational)
 
 simplify :: Expression -> Expression
 simplify (ExpressionUnary operation expression) = case simplify expression, operation of
   ExpressionLiteral value, Neg -> ExpressionLiteral (-value)
   simplifiedExpression, _ -> ExpressionUnary operation simplifiedExpression
-
-simplify (ExpressionBinary Power leftExpression (ExpressionLiteral 1.0)) = simplify leftExpression
-
-simplify (ExpressionBinary Power _ (ExpressionLiteral 0.0)) = ExpressionLiteral 1.0
 
 simplify (ExpressionBinary Power (ExpressionVariable "e") rightExpression) = ExpressionUnary Exp $ simplify rightExpression
 
@@ -31,18 +28,89 @@ simplify (ExpressionBinary operation leftExpression rightExpression) =
 
 simplify expression = expression
 
+trimTimesNodes :: Expression -> Expression -> Maybe Expression
+trimTimesNodes (ExpressionLiteral leftValue) (ExpressionLiteral rightValue) = 
+  if (big 0) == leftNumerator 
+    then Just $ ExpressionLiteral (toRational 0)
+    else if (big 0) == rightNumerator
+      then Just $ ExpressionLiteral (toRational 0)
+      else Nothing
+  where
+    leftNumerator = numerator leftValue
+    rightNumerator = numerator rightValue
+trimTimesNodes (ExpressionLiteral leftValue) rightExpression  = 
+  if leftNumerator == leftDenominator
+    then Just $ rightExpression
+    else Nothing
+  where
+    leftNumerator = numerator leftValue
+    leftDenominator = denominator leftValue
+trimTimesNodes leftExpression (ExpressionLiteral rightValue) = 
+  if rightNumerator == rightDenominator
+    then Just $ leftExpression
+    else Nothing
+  where
+    rightNumerator = numerator rightValue
+    rightDenominator = denominator rightValue
+trimTimesNodes _ _ = Nothing
+
+trimPlusNodes :: Expression -> Expression -> Maybe Expression
+trimPlusNodes (ExpressionLiteral leftValue) rightExpression  = 
+  if (big 0) == leftNumerator 
+    then Just $ rightExpression
+    else Nothing
+  where
+    leftNumerator = numerator leftValue
+trimPlusNodes leftExpression (ExpressionLiteral rightValue) = 
+  if (big 0) == rightNumerator
+    then Just $ leftExpression
+    else Nothing
+  where
+    rightNumerator = numerator rightValue
+trimPlusNodes _ _ = Nothing 
+
+trimMinusNodes :: Expression -> Expression -> Maybe Expression
+trimMinusNodes (ExpressionLiteral leftValue) rightExpression  = 
+  if (big 0) == leftNumerator 
+    then Just $ ExpressionUnary Neg rightExpression
+    else Nothing
+  where
+    leftNumerator = numerator leftValue
+trimMinusNodes leftExpression (ExpressionLiteral rightValue) = 
+  if (big 0) == rightNumerator
+    then Just $ leftExpression
+    else Nothing
+  where
+    rightNumerator = numerator rightValue
+trimMinusNodes _ _ = Nothing 
+
+trimDivideNodes :: Expression -> Expression -> Maybe Expression
+trimDivideNodes (ExpressionLiteral leftValue) rightExpression  = 
+  if (big 0) == leftNumerator 
+    then Just $ ExpressionLiteral $ toRational 0
+    else Nothing
+  where
+    leftNumerator = numerator leftValue
+trimDivideNodes _ _ = Nothing 
+
+trimPowerNodes :: Expression -> Expression -> Maybe Expression
+trimPowerNodes leftExpression (ExpressionLiteral rightValue) = 
+  if rightNumerator == (big 0)
+    then Just $ ExpressionLiteral $ toRational 1
+    else if rightNumerator == rightDenominator
+        then Just $ leftExpression
+        else Nothing
+  where
+    rightNumerator = numerator rightValue
+    rightDenominator = denominator rightValue
+trimPowerNodes _ _ = Nothing 
+
 trimSimpleNodes :: Expression -> Expression -> BinaryOperation -> Maybe Expression
-trimSimpleNodes simplifiedLeftExpression simplifiedRightExpression operation = case simplifiedLeftExpression, simplifiedRightExpression, operation of
-  ExpressionLiteral 0.0, _, Times -> Just $ ExpressionLiteral 0.0
-  _, ExpressionLiteral 0.0, Times -> Just $ ExpressionLiteral 0.0
-  ExpressionLiteral 1.0, _, Times -> Just $ simplifiedRightExpression
-  _, ExpressionLiteral 1.0, Times -> Just $ simplifiedLeftExpression
-  ExpressionLiteral 0.0, _, Plus -> Just $ simplifiedRightExpression
-  _, ExpressionLiteral 0.0, Plus -> Just $ simplifiedLeftExpression
-  ExpressionLiteral 0.0, _, Minus -> Just $ ExpressionUnary Neg simplifiedRightExpression
-  _, ExpressionLiteral 0.0, Minus -> Just $ simplifiedLeftExpression
-  ExpressionLiteral 0.0, _, Divide -> Just $ ExpressionLiteral 0.0
-  _, _, _ -> Nothing
+trimSimpleNodes leftExpression rightExpresson Times = trimTimesNodes leftExpression rightExpresson
+trimSimpleNodes leftExpression rightExpresson Plus = trimPlusNodes leftExpression rightExpresson
+trimSimpleNodes leftExpression rightExpresson Minus = trimMinusNodes leftExpression rightExpresson
+trimSimpleNodes leftExpression rightExpresson Power = trimPowerNodes leftExpression rightExpresson
+trimSimpleNodes _ _ _ = Nothing
 
 trimConstantLeafNodes :: Expression -> Expression -> BinaryOperation -> Maybe Expression
 trimConstantLeafNodes simplifiedLeftExpression simplifiedRightExpression operation = case simplifiedLeftExpression, simplifiedRightExpression, operation of
@@ -50,7 +118,6 @@ trimConstantLeafNodes simplifiedLeftExpression simplifiedRightExpression operati
   ExpressionLiteral leftValue, ExpressionLiteral rightValue, Minus -> Just $ ExpressionLiteral (leftValue - rightValue)
   ExpressionLiteral leftValue, ExpressionLiteral rightValue, Times -> Just $ ExpressionLiteral (leftValue * rightValue)
   ExpressionLiteral leftValue, ExpressionLiteral rightValue, Divide -> Just $ ExpressionLiteral (leftValue / rightValue)
-  ExpressionLiteral leftValue, ExpressionLiteral rightValue, Power -> Just $ ExpressionLiteral (pow leftValue rightValue)
   _, _, _ -> Nothing
 
 trimTimesOperations :: Expression -> Expression -> BinaryOperation -> Maybe Expression
