23cccc63de74df3e37df8f885f2a048e6cd1055a
mikkonecny <mikkonecny@gmail.com>
2020-06-23 19:44:36 +0100

    Apprx sqrt tests, failing due to a bug in sqrtA

--- test/IntervalArith/Approx/Approx.purs
+++ test/IntervalArith/Approx/Approx.purs
@@ -3,6 +3,7 @@ module Test.IntervalArith.Approx
   ) where
 
 import Prelude
+
 import Test.Field (fieldTests)
 import Test.IntervalArith.Approx.Abs (approxTests_AbsA)
 import Test.IntervalArith.Approx.ApproxOrder (approxTests_Consistent, approxTests_ApproxOrder)
@@ -11,6 +12,7 @@ import Test.IntervalArith.Approx.FromRational (approxTests_fromRational, approxT
 import Test.IntervalArith.Approx.NumOrder (approxTests_NumOrder)
 import Test.IntervalArith.Approx.Reductions (approxTests_Reductions)
 import Test.IntervalArith.Approx.ShowA (approxTests_showA)
+import Test.IntervalArith.Approx.Sqrt (approxTests_SqrtA)
 import Test.Unit (TestSuite)
 
 approxTests :: TestSuite
@@ -24,7 +26,7 @@ approxTests = do
   approxTests_NumOrder
   approxTests_AbsA
   approxTests_Field
+  approxTests_SqrtA
 
--- approxTests_SqrtA
 approxTests_Field :: TestSuite
 approxTests_Field = fieldTests approxEqParams
