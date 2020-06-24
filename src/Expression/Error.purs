65e660dda9904d0575a2b907b0dac21c63ef83f1
mikkonecny <mikkonecny@gmail.com>
2020-06-24 13:37:12 +0100

    sqrt throws EvaluationError, MultipleErrors  array

--- src/Expression/Error.purs
+++ src/Expression/Error.purs
@@ -1,24 +1,28 @@
 module Expression.Error where
 
 import Prelude
+import Data.Either (Either(..))
+import Data.String (joinWith)
 import Expression.Syntax (VariableName)
 
-import Data.Either (Either(..))
-
-data Error = ParseError String
-           | UnknownValue VariableName
-           | MultipleErrors String
-           | UnsupportedOperation String
+data Error
+  = ParseError String
+  | UnknownValue VariableName
+  | EvaluationError String
+  | UnsupportedOperation String
+  | MultipleErrors (Array Error)
 
 instance showError :: Show Error where
   show (ParseError s) = "Parse error: " <> s
   show (UnknownValue n) = "Unknown value: " <> n
+  show (EvaluationError n) = "Evaluation error: " <> n
   show (UnsupportedOperation n) = "Unsupported operation: " <> n
-  show (MultipleErrors msg) = msg     
+  show (MultipleErrors errors) = joinWith " | " (map show errors)
 
 derive instance eqError :: Eq Error
 
-type Expect a = Either Error a
+type Expect a
+  = Either Error a
 
 throw :: forall a. Error -> Expect a
 throw = Left
@@ -29,8 +33,11 @@ parseError = throw <<< ParseError
 unknownValue :: forall a. VariableName -> Expect a
 unknownValue = throw <<< UnknownValue
 
-multipleErrors :: forall a. String -> Expect a
-multipleErrors = throw <<< MultipleErrors
-
 unsupportedOperation :: forall a. String -> Expect a
 unsupportedOperation = throw <<< UnsupportedOperation
+
+evaluationError :: forall a. String -> Expect a
+evaluationError = throw <<< EvaluationError
+
+multipleErrors :: forall a. Array Error -> Expect a
+multipleErrors = throw <<< MultipleErrors
