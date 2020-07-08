ba5a87124419306515158a21af72d07d45cd5897
mikkonecny <mikkonecny@gmail.com>
2020-07-08 18:00:35 +0100

    utils for 4-tuples and lazy lists

---
+++ src/Misc/Tuple.purs
@@ -0,0 +1,11 @@
+module Misc.Tuple where
+
+import Data.Tuple (Tuple(..))
+
+type Tuple22 a b c d
+  = Tuple (Tuple a b) (Tuple c d)
+
+tuple22 ::
+  forall a b c d.
+  b -> a -> d -> c -> Tuple (Tuple b a) (Tuple d c)
+tuple22 a b c d = Tuple (Tuple a b) (Tuple c d)
