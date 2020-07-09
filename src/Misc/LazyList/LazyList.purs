3b8e9bdfd85dee35e77a83c3f5bf5a73b1ede362
mikkonecny <mikkonecny@gmail.com>
2020-07-09 17:04:49 +0100

    Approx sinA: fix loops in lazy lists

--- src/Misc/LazyList/LazyList.purs
+++ src/Misc/LazyList/LazyList.purs
@@ -1,12 +1,10 @@
 module Misc.LazyList where
 
-import Data.List.Lazy (List, Step(..))
+import Data.List.Lazy (List)
 import Data.List.Lazy as L
 import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(..))
 import Effect.Exception.Unsafe (unsafeThrow)
-import Halogen.HTML (a)
-import Web.HTML.Event.EventTypes (offline)
 
 unsafeIndex :: forall a. List a -> Int -> a
 unsafeIndex list i = case list L.!! i of
@@ -25,18 +23,6 @@ unsafeTail list = case L.tail list of
   Just as -> as
   Nothing -> unsafeThrow "unsafeTail: empty list"
 
-data EmptySingletonMore a =
-  Empty | Singleton a | More a a (List a)
-
-emptySingletonMore :: forall a. List a -> EmptySingletonMore a
-emptySingletonMore xs =
-  case L.step xs of
-    Nil -> Empty
-    Cons x1 xs1 ->
-      case L.step xs1 of
-        Nil -> Singleton x1
-        (Cons x2 rest) -> More x1 x2 rest
-
 zipWith4 ::
   forall a b c d e.
   (a -> b -> c -> d -> e) ->
@@ -46,4 +32,3 @@ zipWith4 f l1 l2 l3 l4 =
     (\(Tuple a1 a2) (Tuple a3 a4) -> f a1 a2 a3 a4)
     (L.zip l1 l2)
     (L.zip l3 l4)
-
--- src/Misc/LazyList/NumberSequences.purs
+++ src/Misc/LazyList/NumberSequences.purs
@@ -1,10 +1,10 @@
 module Misc.LazyList.NumberSequences where
 
 import Prelude
+import Data.Int (even)
 import Data.List.Lazy (List, (:))
 import Data.List.Lazy as L
-import Effect.Exception.Unsafe (unsafeThrow)
-import Misc.LazyList (EmptySingletonMore(..), emptySingletonMore)
+import Data.Tuple (fst, snd)
 
 ones :: forall a. Semiring a => List a
 ones = L.repeat one
@@ -18,19 +18,12 @@ powers x = L.iterate (_ * x) one
 
 -- | A list of factorial values [1,1,2,6,24,...].
 factorials :: forall a. Semiring a => List a
-factorials = myscan one onetwoetc
-  where
-  myscan u l =
-    case L.step l of
-      L.Cons x xs -> u : myscan (u * x) xs
-      L.Nil -> unsafeThrow "factorials: impossible"
+factorials = one : L.scanrLazy (*) one onetwoetc
 
 -- test = L.take 0 (factorials :: List Int)
-
 -- | A list of the factorial values of odd numbers.
 oddFactorials :: forall a. Semiring a => List a
-oddFactorials = pickOdd factorials
-  where
-  pickOdd xs = case emptySingletonMore xs of
-    More _ x rest -> x : (pickOdd rest)
-    _ -> unsafeThrow "oddFactorials: impossible"
+oddFactorials =
+  map snd
+    $ L.filter (even <<< fst)
+    $ L.zip (onetwoetc :: List Int) factorials
