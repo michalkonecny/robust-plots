e411b8ba634514c06a270e1b4a8a723a235b44c3
mikkonecny <mikkonecny@gmail.com>
2020-07-14 00:21:32 +0100

    automatic second derivative evaluation

--- src/Expression/AutomaticDifferentiator.purs
+++ src/Expression/AutomaticDifferentiator.purs
@@ -1,7 +1,6 @@
 module Expression.AutomaticDifferentiator where
 
 import Prelude
-
 import Data.Maybe (Maybe(..))
 import Data.Number (isNaN)
 import Data.Tuple (Tuple(..))
@@ -133,3 +132,114 @@ evaluateDerivative variableMap = eval
           tanU = tan u
         in
           pure { value: tanU, derivative: u' * (one + tanU ^ 2) }
+
+type ValueAndDerivative2 a
+  = { value :: a, derivative :: a, derivative2 :: a }
+
+evaluateDerivative2 ::
+  forall a.
+  (Field a) =>
+  (HasRational a) =>
+  (HasIsZero a) =>
+  (HasSqrt a) =>
+  (HasPower a) =>
+  (HasSinCos a) =>
+  (HasExpLog a) =>
+  (VariableMap (ValueAndDerivative2 a)) ->
+  Expression ->
+  Expect (ValueAndDerivative2 a)
+evaluateDerivative2 variableMap = eval
+  where
+  eval = case _ of
+    ExpressionLiteral valueR -> do
+      value <- fromRational valueR
+      pure { value, derivative: zero, derivative2: zero }
+    ExpressionVariable name -> case lookup variableMap name of
+      Just valueAndDerivative2 -> pure valueAndDerivative2
+      _ -> unknownValue name
+    ExpressionBinary operation leftExpression rightExpression -> evalBinary operation leftExpression rightExpression
+    ExpressionUnary operation subExpression -> evalUnary operation subExpression
+    ExpressionLet name expression parentExpression -> do
+      expressionValueAndDerivative2 <- eval expression
+      evaluateDerivative2 ([ (Tuple name expressionValueAndDerivative2) ] <> variableMap) parentExpression
+
+  evalBinary operation leftExpression rightExpression = do
+    { value: u, derivative: u', derivative2: u'' } <- eval leftExpression
+    { value: v, derivative: v', derivative2: v'' } <- eval rightExpression
+    case operation of
+      Plus -> pure { value: u + v, derivative: u' + v', derivative2: u'' + v'' }
+      Minus -> pure { value: u - v, derivative: u' - v', derivative2: u'' - v'' }
+      Times ->
+        pure
+          { value: u * v
+          , derivative: u * v' + u' * v
+          , derivative2: u * v'' + two * (u' * v') + u'' * v
+          }
+      Divide ->
+        pure
+          { value: u / v
+          , derivative: (u' * v - u * v') / (v ^ 2)
+          , derivative2: (u'' * v ^ 2 - v * (two * u' * v' + u * v'') + two * u * v' ^ 2) / (v ^ 3)
+          }
+      -- (g^f)' = g^(f-1) * ((f*g')+(g*f'*log(g)))
+      -- source: https://www.wolframalpha.com/input/?i=(f%5E(g))%27
+      Power -> do
+        uPowV <- u `power` v
+        logU <- log u
+        let
+          t = (v * u') / u + logU * v'
+        pure
+          { value: uPowV
+          , derivative: uPowV * t
+          , derivative2: uPowV * (t ^ 2 + (v * u'' + two * u' * v') / u - v * u' ^ 2 / u ^ 2 + logU * v'')
+          }
+
+  evalUnary operation subExpression = do
+    { value: u, derivative: u', derivative2: u'' } <- eval subExpression
+    case operation of
+      Neg -> pure { value: -u, derivative: -u', derivative2: -u'' }
+      Sqrt -> do
+        sqrtU <- sqrt u
+        pure
+          { value: sqrtU
+          , derivative: u' / (two * sqrtU)
+          , derivative2: (two * u * u'' - u' ^ 2) / (two * u * two * sqrtU)
+          }
+      -- (sqrt(f))' = f'/(2*sqrt(f))
+      Exp ->
+        let
+          expU = exp u
+        in
+          pure { value: expU, derivative: u' * expU, derivative2: expU * (u'' + u' ^ 2) }
+      Log -> do
+        logU <- log u
+        pure { value: logU, derivative: u' / u, derivative2: (u * u'' - u' ^ 2) / u ^ 2 }
+      Sine ->
+        let
+          sinU = sin u
+
+          cosU = cos u
+        in
+          pure { value: sinU, derivative: u' * cosU, derivative2: u'' * cosU - (u' ^ 2) * sinU }
+      Cosine ->
+        let
+          sinU = sin u
+
+          cosU = cos u
+        in
+          pure { value: cosU, derivative: -u' * sinU, derivative2: -u'' * sinU - (u' ^ 2) * cosU }
+      -- tan(f)' = f' * (1 + tan^2(f)) 
+      Tan ->
+        let
+          tanU = tan u
+
+          cos2U = cos (two * u)
+        in
+          pure
+            { value: tanU
+            , derivative: u' * (one + tanU ^ 2)
+            , derivative2: two * (u'' + two * u' ^ 2 * tanU) / (cos2U + one)
+            }
+
+t2 :: Expression
+t2 = ExpressionBinary Power (ExpressionVariable "x") (ExpressionLiteral two)
