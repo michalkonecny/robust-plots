b72a0567c6975b8d436816bbef907c42ecbc9c13
RelativeForce <joshiceddy@hotmail.com>
2020-07-16 15:58:42 +0100

    Renamed function

--- test/FFI/Timer.purs
+++ test/FFI/Timer.purs
@@ -1,18 +1,18 @@
 module FFI.Timer where
 
 import Prelude
+
 import Effect (Effect)
-import Effect.Aff (Aff)
 import Effect.Class (liftEffect)
-import Test.Unit (failure)
+import Test.Unit (Test, failure)
 
 foreign import processTime :: Effect Number
 
 foreign import processTimeElapsedSince :: Number -> Effect Number
 
-runPerformance :: Number -> Aff Unit -> Aff Unit
-runPerformance time job = do
+performanceTest :: Number -> Test -> Test
+performanceTest time job = do
   start <- liftEffect processTime
   job
-  e <- liftEffect $ processTimeElapsedSince start
-  when (e > time) $ failure $ "expected completion in " <> (show time) <> "ms, actually completed in " <> (show e) <> "ms"
+  elapsed <- liftEffect $ processTimeElapsedSince start
+  when (elapsed > time) $ failure $ "expected completion in " <> (show time) <> "ms, actually completed in " <> (show elapsed) <> "ms"
--- test/IntervalArith/Approx/Pi.purs
+++ test/IntervalArith/Approx/Pi.purs
@@ -4,7 +4,7 @@ module Test.IntervalArith.Approx.Pi
 
 import Prelude
 
-import FFI.Timer (runPerformance)
+import FFI.Timer (performanceTest)
 import IntervalArith.Approx (consistent)
 import IntervalArith.Approx.Pi (piA)
 import IntervalArith.Approx.ShowA (showA)
@@ -18,7 +18,7 @@ approxTests_piA :: TestSuite
 approxTests_piA =
   suite "IntervalArith.Approx - pi" do
     test "SHOULD HOLD piA 200 = 3.14159265358979323846264338327950288419716939937510582097494~" do
-      runPerformance 0.2 do
+      performanceTest 0.2 do
         let
           -- given
           input1 = piA 200
