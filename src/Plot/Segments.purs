1e261875482ee5bc017f0918be0ba9defccc323a
mikkonecny <mikkonecny@gmail.com>
2020-07-16 01:47:34 +0100

    improve segmentation for partial functions

--- src/Plot/Segments.purs
+++ src/Plot/Segments.purs
@@ -1,9 +1,11 @@
 module Plot.Segments where
 
 import Prelude
+
 import Data.Array (fromFoldable)
 import Data.List (List, singleton)
 import Data.Maybe (Maybe(..))
+import Data.Number as Number
 import Data.Ord (abs)
 import Expression.Evaluate.AutomaticDifferentiator (ValueAndDerivative2)
 import IntervalArith.Approx (Approx, fromRationalBoundsPrec)
@@ -41,27 +43,31 @@ segmentDomain accuracyTarget evaluator l u = fromFoldable $ segementDomainF 0 l 
           segmentBasedOnDerivative
             { depth, lower, mid, upper }
             x
-            (evaluator (rationalToNumber lower))
-            (evaluator (rationalToNumber upper))
-            (evaluator a1)
-            (evaluator a2)
-            (evaluator (rationalToNumber mid))
+            (checkFinite $ evaluator (rationalToNumber lower) <#> (_.value))
+            (checkFinite $ evaluator (rationalToNumber upper) <#> (_.value))
+            (checkFinite $ evaluator a1 <#> (_.derivative2))
+            (checkFinite $ evaluator a2 <#> (_.derivative2))
+            (checkFinite $ evaluator (rationalToNumber mid) <#> (_.derivative))
 
-  segmentBasedOnDerivative :: { depth :: Int, lower :: Rational, mid :: Rational, upper :: Rational } -> Approx -> Maybe (ValueAndDerivative2 Number) -> Maybe (ValueAndDerivative2 Number) -> Maybe (ValueAndDerivative2 Number) -> Maybe (ValueAndDerivative2 Number) -> Maybe (ValueAndDerivative2 Number) -> List Approx
-  segmentBasedOnDerivative state@{ depth, lower, mid, upper } x _ _ (Just a1) (Just a2) (Just b) =
+  segmentBasedOnDerivative state@{ depth, lower, mid, upper } x (Just _) (Just _) (Just a1) (Just a2) (Just b) =
     let
       w = rationalToNumber $ mid - lower
 
-      a = (a1.derivative2 + a2.derivative2) / two
+      a = (a1 + a2) / two
 
-      h = if abs b.derivative > one then abs ((a * w * w) / b.derivative) else abs (a * w * w)
+      h = if abs b > one then abs ((a * w * w) / b) else abs (a * w * w)
     in
-      if abs (a1.derivative2 - a2.derivative2) * w > 3.0 * accuracyTarget || h > accuracyTarget then
+      if abs (a1 - a2) * w > 3.0 * accuracyTarget || h > accuracyTarget then
         bisect state
       else
         singleton x
 
   -- function not defined on either end, assume not defined on the whole segment:
-  segmentBasedOnDerivative state x Nothing Nothing Nothing Nothing Nothing = singleton x
+  segmentBasedOnDerivative state x Nothing Nothing _ _ _ = singleton x
 
   segmentBasedOnDerivative state x _ _ _ _ _ = bisect state
+
+checkFinite :: Maybe Number -> Maybe Number
+checkFinite ma@(Just a) = if Number.isFinite a then ma else Nothing
+
+checkFinite ma = ma
