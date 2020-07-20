aa56fcda8efe959b8f12a15ef38924376ccbe229
mikkonecny <mikkonecny@gmail.com>
2020-07-20 09:51:23 +0100

    segmentation checks also f' stability this is needed for abs, min, max

--- src/Plot/Segments.purs
+++ src/Plot/Segments.purs
@@ -1,7 +1,6 @@
 module Plot.Segments where
 
 import Prelude
-
 import Data.Array (fromFoldable)
 import Data.List (List, singleton)
 import Data.Maybe (Maybe(..))
@@ -27,11 +26,15 @@ segmentDomain accuracyTarget evaluator l u = fromFoldable $ segementDomainF 0 l 
 
     mid = (lower + upper) / two
 
-    range = upper - lower
+    width = upper - lower
 
-    a1 = rationalToNumber $ lower + (range / three)
+    xL = rationalToNumber lower
 
-    a2 = rationalToNumber $ lower + ((range * two) / three)
+    xU = rationalToNumber upper
+
+    xL3 = rationalToNumber $ lower + (width / three)
+
+    xU3 = rationalToNumber $ lower + ((width * two) / three)
 
     segments =
       if depth < 5 then
@@ -43,29 +46,41 @@ segmentDomain accuracyTarget evaluator l u = fromFoldable $ segementDomainF 0 l 
           segmentBasedOnDerivative
             { depth, lower, mid, upper }
             x
-            (checkFinite $ evaluator (rationalToNumber lower) <#> (_.value))
-            (checkFinite $ evaluator (rationalToNumber upper) <#> (_.value))
-            (checkFinite $ evaluator a1 <#> (_.derivative2))
-            (checkFinite $ evaluator a2 <#> (_.derivative2))
-            (checkFinite $ evaluator (rationalToNumber mid) <#> (_.derivative))
+            { fxL: checkFinite $ evaluator xL <#> (_.value)
+            , fxU: checkFinite $ evaluator xU <#> (_.value)
+            , f'xL: (checkFinite $ evaluator xL <#> (_.derivative))
+            , f'xU: (checkFinite $ evaluator xU <#> (_.derivative))
+            , f''xL3: (checkFinite $ evaluator xL3 <#> (_.derivative2))
+            , f''xU3: (checkFinite $ evaluator xU3 <#> (_.derivative2))
+            }
 
-  segmentBasedOnDerivative state@{ depth, lower, mid, upper } x (Just _) (Just _) (Just a1) (Just a2) (Just b) =
+  segmentBasedOnDerivative state@{ depth, lower, mid, upper } x { fxL: Just _, fxU: Just _, f'xL: Just b1, f'xU: Just b2, f''xL3: Just a1, f''xU3: Just a2 } =
     let
       w = rationalToNumber $ mid - lower
 
-      a = (a1 + a2) / two
+      bUnstable = abs (b1 - b2) * w > accuracyTarget
 
-      h = if abs b > one then abs ((a * w * w) / b) else abs (a * w * w)
+      aUnstable = abs (a1 - a2) * w > 3.0 * accuracyTarget
     in
-      if abs (a1 - a2) * w > 3.0 * accuracyTarget || h > accuracyTarget then
+      if bUnstable || aUnstable then
         bisect state
       else
-        singleton x
+        let
+          b = (b1 + b2) / two
+
+          a = (a1 + a2) / two
+
+          h = if abs b > one then abs ((a * w * w) / b) else abs (a * w * w)
+        in
+          if h > accuracyTarget then
+            bisect state
+          else
+            singleton x
 
   -- function not defined on either end, assume not defined on the whole segment:
-  segmentBasedOnDerivative state x Nothing Nothing _ _ _ = singleton x
+  segmentBasedOnDerivative state x { fxL: Nothing, fxU: Nothing } = singleton x
 
-  segmentBasedOnDerivative state x _ _ _ _ _ = bisect state
+  segmentBasedOnDerivative state x _ = bisect state
 
 checkFinite :: Maybe Number -> Maybe Number
 checkFinite ma@(Just a) = if Number.isFinite a then ma else Nothing
