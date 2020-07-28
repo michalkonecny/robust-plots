ee4ac9da67ce8672282c5ec97c16bba2794ee612
RelativeForce <joshiceddy@hotmail.com>
2020-07-28 11:56:19 +0100

    Update Main.purs

--- src/Components/Main/Main.purs
+++ src/Components/Main/Main.purs
@@ -165,7 +165,7 @@ mainComponent =
             [ className "page-footer font-small fixed-bottom" ]
             [ HH.footer
                 [ className "footer-copyright text-right py-3 pr-2" ]
-                [ HH.text "version 0.1.0, ©2020 Michal Konecny, Joshua Eddy; source on"
+                [ HH.text "Version 0.1.0, ©2020 Michal Konecny, Joshua Eddy; source on"
                 , HH.a
                     [ HP.href "https://github.com/michalkonecny/robust-plots"
                     , className "pl-1"
